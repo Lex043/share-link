dc3aaa5d7e203381b993871eda4e1d12e4eb9678
Oyebamiji Alex <alexferanmi390@gmail.com>
2024-09-30 20:57:01 +0100

    chore: implementing links layout

---
+++ app/components/create-link.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { linkStore } from "@/store/link";
+import LinkInput from "./link-input";
+import useCrudLink from "../hooks/useCrudLink";
+
+export default function CreateLink() {
+    const links = linkStore((state) => state.links);
+
+    return (
+        <section className="relative flex flex-col gap-6">
+            {links &&
+                links.map((link, index) => (
+                    <section
+                        className="rounded-xl bg-light-grey p-5"
+                        key={link.id}
+                    >
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center gap-1 text-base font-bold text-grey">
+                                <div className="flex items-center gap-1">
+                                    <span>=</span>
+                                    <h1>Link</h1>
+                                </div>
+                                <span>#{index + 1}</span>
+                            </div>
+                            <p className="text-base text-grey">Remove</p>
+                        </div>
+                        <div className="py-3">
+                            <LinkInput />
+                        </div>
+                        <div className="relative py-3">
+                            {" "}
+                            <LinkInput />
+                        </div>
+                    </section>
+                ))}
+        </section>
+    );
+}
